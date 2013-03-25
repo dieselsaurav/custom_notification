ccee6c8ab4a29f73df7c60a577676a3c467d6f9b
Kumar Saurav <saurav@nwplyng.com>
2013-03-26 04:59:27 +0530

    First Commit

---
+++ src/com/quora/kumarsaurav/customnotification/NotifyMe.java
@@ -0,0 +1,54 @@
+package com.quora.kumarsaurav.customnotification;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RemoteViews;
+
+public class NotifyMe extends Activity {
+
+    final static int NOTIFY_SIMPLE = 1;
+    final static int NOTIFY_CUSTOM = 2;
+    Button mSimpleNotificationButton;
+    Button mCustomNotificationButton;
+    NotificationManager mNotificationManager;
+    Notification.Builder mNotificationBuilder;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notify_me);
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mSimpleNotificationButton = (Button) findViewById(R.id.button_notify_simple);
+        mCustomNotificationButton = (Button) findViewById(R.id.button_notify_custom);
+
+        mSimpleNotificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mNotificationBuilder = new Notification.Builder(NotifyMe.this);
+                mNotificationBuilder.setSmallIcon(R.drawable.ic_launcher);
+                mNotificationBuilder.setContentTitle("Hello World");
+                mNotificationBuilder.setContentInfo("Hey, Everyone");
+                mNotificationManager.notify(NOTIFY_SIMPLE, mNotificationBuilder.build());
+            }
+        });
+
+        mCustomNotificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.notification_like);
+                remoteViews.setTextViewText(R.id.tv_notification_like, "Hello, World");
+                remoteViews.setTextViewCompoundDrawablesRelative(R.id.tv_notification_like, R.drawable.ic_launcher, 0, 0, 0);
+                mNotificationBuilder = new Notification.Builder(NotifyMe.this);
+                mNotificationBuilder.setSmallIcon(R.drawable.ic_launcher);
+                mNotificationBuilder.setContent(remoteViews);
+                mNotificationManager.notify(NOTIFY_CUSTOM, mNotificationBuilder.build());
+            }
+        });
+    }
+
+}
