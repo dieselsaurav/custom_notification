be1969a09e6c80ee458928d4b79126f3e79a2d13
Kumar Saurav <saurav@nwplyng.com>
2013-03-26 08:50:59 +0530

    follow notification

--- src/com/quora/kumarsaurav/customnotification/NotifyMe.java
+++ src/com/quora/kumarsaurav/customnotification/NotifyMe.java
@@ -1,13 +1,15 @@
 package com.quora.kumarsaurav.customnotification;
 
 import android.app.Activity;
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RemoteViews;
+
+import static android.app.Notification.*;
+import static android.view.View.OnClickListener;
 
 public class NotifyMe extends Activity {
 
@@ -16,7 +18,7 @@ public class NotifyMe extends Activity {
     Button mSimpleNotificationButton;
     Button mCustomNotificationButton;
     NotificationManager mNotificationManager;
-    Notification.Builder mNotificationBuilder;
+    Builder mNotificationBuilder;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,10 +28,10 @@ public class NotifyMe extends Activity {
         mSimpleNotificationButton = (Button) findViewById(R.id.button_notify_simple);
         mCustomNotificationButton = (Button) findViewById(R.id.button_notify_custom);
 
-        mSimpleNotificationButton.setOnClickListener(new View.OnClickListener() {
+        mSimpleNotificationButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                mNotificationBuilder = new Notification.Builder(NotifyMe.this);
+                mNotificationBuilder = new Builder(NotifyMe.this);
                 mNotificationBuilder.setSmallIcon(R.drawable.ic_launcher);
                 mNotificationBuilder.setContentTitle("Hello World");
                 mNotificationBuilder.setContentInfo("Hey, Everyone");
@@ -37,13 +39,13 @@ public class NotifyMe extends Activity {
             }
         });
 
-        mCustomNotificationButton.setOnClickListener(new View.OnClickListener() {
+        mCustomNotificationButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.notification_like);
-                remoteViews.setTextViewText(R.id.tv_notification_like, "Hello, World");
-                remoteViews.setTextViewCompoundDrawablesRelative(R.id.tv_notification_like, R.drawable.ic_launcher, 0, 0, 0);
-                mNotificationBuilder = new Notification.Builder(NotifyMe.this);
+                RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.notification_follow);
+                remoteViews.setTextViewText(R.id.tv_notification_follow, "Hello, World");
+                remoteViews.setTextViewCompoundDrawablesRelative(R.id.tv_notification_follow, R.drawable.ic_launcher, 0, 0, 0);
+                mNotificationBuilder = new Builder(NotifyMe.this);
                 mNotificationBuilder.setSmallIcon(R.drawable.ic_launcher);
                 mNotificationBuilder.setContent(remoteViews);
                 mNotificationManager.notify(NOTIFY_CUSTOM, mNotificationBuilder.build());
